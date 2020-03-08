ae7a32073b4b2844344e1702fd0a892de7507344
HenriArno <61839025+HenriArno@users.noreply.github.com>
2020-03-08 17:58:56 +0100

    Update SMWA_scrape.R

--- SMWA_scrape.R
+++ SMWA_scrape.R
@@ -15,27 +15,34 @@ p_load(httr,rtweet,tidyverse,textclean, textstem, sentimentr, lexicon, maps, dpl
 source('tokensandkeys.R')
 get_token() #test if it works
 
-# Scrape and write to CSV
 
-# Get the tweets
-tweets <- search_tweets("#corona", n = 1000, include_rts = FALSE, 
-              token = get_token())
-tweets <- tibble(tweets$user_id, tweets$text)
-colnames(tweets) <- c('user_id', 'text')
-
-# Remove duplicates (but first remove html links)
+# remove HTML tags
 removeHTML <- function(text) {
   clean_texts <- text %>%
     replace_html( ) %>%
     gsub("http.*", "", .)# remove html links replace_html() also works
-    return(clean_texts)
+  return(clean_texts)
 }  
+
+
+
+# scrape hastags
+tweets <- search_tweets("#corona", n = 1000, include_rts = FALSE, token = get_token())
+tweets <- tibble(tweets$user_id, tweets$text, tweets$created_at, 
+                 tweets$screen_name, tweets$location, timeline = F)
+colnames(tweets) <- c('user_id', 'text', 'created_at', 'screen_name', 'location', 'timeline')
 tweets$text <- tweets$text %>% removeHTML()
 tweets <- tweets %>% distinct()
 
 
-# Write to CSV
-write_csv(tweets, 'dataset.csv', append = T)
+# scrape timelines
+milan <- get_timeline(user = 'MiAirports', n = 3200, token = get_token())
+milan <- tibble(milan$user_id, milan$text, milan$created_at, 
+                milan$screen_name, milan$location, timeline = T)
+colnames(milan) <- c('user_id', 'text', 'created_at', 'screen_name', 'location', 'timeline')
+milan$text <- milan$text %>% removeHTML()
+milan <- milan %>% distinct()
 
 
-
+to_dataset <- bind_rows(tweets, milan)
+write_csv(to_dataset, 'dataset.csv', append = T)
