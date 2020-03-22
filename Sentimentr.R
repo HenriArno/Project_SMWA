d53f790ec9f9c75e6062fa78cf9b678f567b43c1
Pieter <pieter.gobin@gmail.com>
2020-03-22 17:47:39 +0100

    Sentiment R is implemented both with updated valence shifters table and without

---
+++ Sentimentr.R
@@ -0,0 +1,60 @@
+
+# Sentiment Analysis using Sentimentr package -----------------------------
+#In this script we will analyse our cleaned tweets to check for sentiment using the sentiment r package
+
+
+# Setting wd and clearing environmnet -------------------------------------
+
+rm(list=ls())
+
+library(rstudioapi)
+#sets working directory to file directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+# loading packages --------------------------------------------------------
+if (!require("pacman")) install.packages("pacman") ; require("pacman")
+p_load(httr,rtweet,tidyverse,textclean, textstem, sentimentr, lexicon)
+
+
+
+# loading data ------------------------------------------------------------
+data = read.csv("dataset_cleaned.csv")
+#slice data to make code run faster for debug (TEMPORARY)
+data <- data%>% slice(1:200)
+#Select text to perform analysis on
+text <- as.vector(data$text)
+
+
+# perform sentiment analyis -----------------------------------------------
+
+#get the sentiment
+sentiment <- text %>% get_sentences()%>%sentiment_by()
+#make matching index columns
+sentiment$X <-  sentiment$element_id
+#join everything in tibble to get a clear result
+result <- sentiment%>%select(-c(element_id, sd))%>% merge(data, by = 'X') %>% select(-X)
+
+
+
+# Sentiment with punctuation ----------------------------------------------
+#We are not looking at punctuation in the previous case however, 
+#which is why we will update the Valence shifters table here
+
+#set question mark and exclamation mark to the amplifier class of valence shifter (=2) and update table
+update <- data.frame(x=c('exclamation', 'question mark'), y = c(2,2))
+valence_shifters_updated <-update_valence_shifter_table(key = hash_valence_shifters,
+                                                        x = update)
+
+#replace punctuation in text with their respective textual counterpart
+text <- text %>% 
+  gsub('!', 'exclamation') %>%
+  gsub('?', 'question mark')
+
+#perform analysis
+sentiment2 <- text %>% get_sentences()%>%sentiment_by(valence_shifters_dt = valence_shifters_updated)
+#make matching index columns
+sentiment2$X <-  sentiment2$element_id
+#join everything in tibble to get a clear result
+result2 <- sentiment2%>%select(-c(element_id, sd))%>% merge(data, by = 'X') %>% select(-X)
+
