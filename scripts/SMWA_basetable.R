46bc5fb38361e6e90839df940a6ab224501cd19c
Arfibo <afribo@MacBook-Pro-van-Arfibo.local>
2020-03-24 15:29:53 +0100

    BASETABLE KLAAAARRRRR

--- scripts/SMWA_basetable.R
+++ scripts/SMWA_basetable.R
@@ -48,12 +48,37 @@ basetable <- basetable[-c(1:633),]
 
 
 # add dependent and average sentiment variable
+# hardcoding to get basetable quickly for prediction
+# Will be changed soon
+dates <- unique(basetable$timestamp)[1:13]
+dependent_data <- dependent_data[5:17,]
+dependent_data$timestamp <- dates
 
+dates[14] = "2020-03-24"
+sentiment_day_data$timestamp <- dates
+
+basetable <- basetable %>% 
+  merge(., sentiment_day_data, by = 'timestamp') %>%
+  merge(., dependent_data, by = 'timestamp')
+
+rm(dependent_data, sentiment_day_data)
 
 # rename columns and rearrange column order
+basetable$dates <- NULL
+basetable$day <- NULL
+basetable$day.x <- NULL
+basetable$day.y <- NULL
+basetable$negations.y <- NULL
+
+basetable <- basetable[c('text', 'timestamp', 'user_id', 'screenname', 'location', 'sentiment', 'ave_sentiment.x',
+                         'ave_sentiment.y', 'negations.x', 'best_topic', 'best_topic_gamma', 'topic_1_dummy', 
+                         'topic_2_dummy', 'topic_3_dummy', 'topic_4_dummy', 'topic_1_gamma', 'topic_2_gamma',
+                         'topic_3_gamma', 'topic_4_gamma','word_count.x','word_count.y', 'cancelled.flights')]
   
-  
-  
+colnames(basetable) <- c('text', 'timestamp', 'user_id', 'screenname', 'location', 'sentiment_dict', 'sentimentr_1',
+                         'sentimentr_2', 'sentiment_dict_daily_avg', 'best_topic', 'best_topic_gamma', 'topic_1_dummy', 
+                         'topic_2_dummy', 'topic_3_dummy', 'topic_4_dummy', 'topic_1_gamma', 'topic_2_gamma',
+                         'topic_3_gamma', 'topic_4_gamma','sentimentr_1_wordc','sentimentr_2_wordc', 'cancellations')
 # Write basetable to CSV --------------------------------------------------
 basetable %>% write.csv(., "./sources/cleaned/basetable.csv")
 
